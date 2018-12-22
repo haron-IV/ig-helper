5c3d0747626fca0b22d6dce1c05fa450ee42edd7
MABBYN <mabbyn@MacBook-Pro-MABBYN.local>
2018-12-22 19:32:09 +0100

    create basic sending form.

--- bot/bot.js
+++ bot/bot.js
@@ -1,18 +1,33 @@
 const textarea = document.querySelector('#botTextarea');
 const button = document.querySelector('#sendButton');
+const showButton = document.querySelector('#showButton');
+const showArea = document.querySelector('#snapchats');
 
 const apiRoute = 'http://localhost:8080/api';
 
 let txt;
 
 function getSnapchats(){
-    txt = textarea.value;
-    txt = txt.split(',');
+    txt = textarea.value.toString;
+    console.log(txt)
+}
+
+function showSnapchats(){
+    axios.get(`${apiRoute}/snapchats`, {title: 'test', snapchats: txt})
+    .then(function (response) {
+      textarea.value = '';
+    
+      showArea.innerHTML = response.toString();
+      console.log(response)
+    })
+    .catch(function (error) {
+		console.log(error)
+    });
 }
 
 function sendData(){
 
-    axios.post(`${apiRoute}/snapchats`, {title: 'test', snapchats: txt})
+    axios.post(`${apiRoute}/snapchats`, {title: 'girl', snapchats: txt})
     .then(function (response) {
       console.log(response)
     })
@@ -24,4 +39,8 @@ function sendData(){
 button.addEventListener('click', () => {
     getSnapchats();
     sendData();
+})
+
+showButton.addEventListener('click', () => {
+    showSnapchats();
 })
