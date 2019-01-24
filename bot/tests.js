9b6a3e2e10b6612ce30c14d7779dab6dfe1258b6
MABBYN <mabbyn@MacBook-Pro-MABBYN.local>
2019-01-25 00:06:31 +0100

    set more confortable test logs

--- bot/tests.js
+++ bot/tests.js
@@ -3,33 +3,66 @@ let testsResults = {
         test_BotDataStats: null,
         test_globalDataBot: null,
         test_CalendarData: null
-    }
+    },
+
+
+    failedTests: [],
+
+    passed: 0,
+    failed: 0
+}
+
+function addOnePassed () {
+    testsResults.passed++;
+}
+
+function addOneFailed () {
+    testsResults.failed++;
+}
+
+function addFailedTestName (test_name) {
+    testsResults.failedTests.push(test_name)
 }
 
 // -------------------------------------------------------------------------------------------------------------- \\
 //                                             localStorage tests
 
 function test_BotDataStats () {
+    const test_name = 'test_BotDataStats';
+
     if ( localStorage.getItem('CalendarData') ) {
         testsResults.localStorageTests.test_BotDataStats = true;
+        addOnePassed();
     } else {
         testsResults.localStorageTests.test_BotDataStats = false;
+        addOneFailed();
+        addFailedTestName(test_name);
     }
 }
 
 function test_globalDataBot () {
+    const test_name = 'test_globalDataBot';
+
     if ( localStorage.getItem('GlobalDataBot') ) {
         testsResults.localStorageTests.test_globalDataBot = true;
+        addOnePassed();
     } else {
         testsResults.localStorageTests.test_globalDataBot = false;
+        addOneFailed();
+        addFailedTestName(test_name);
     }
 }
 
 function test_CalendarData () {
+    const test_name = 'test_CalendarData';
+
     if ( localStorage.getItem('CalendarData') ) {
         testsResults.localStorageTests.test_CalendarData = true;
+        addOnePassed();
     } else {
         testsResults.localStorageTests.test_CalendarData = false;
+        addOneFailed();
+        addFailedTestName(test_name);
     }
 }
 
@@ -42,6 +75,9 @@ function initLocalStorageTests () {
 //                                            END localStorage tests
 // -------------------------------------------------------------------------------------------------------------- \\
 
+function showTestLog () {
+    const tr = testsResults;
+}
 
 function initTests () {
     initLocalStorageTests();
