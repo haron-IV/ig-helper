e066a9280e089ba40e9108e7d83d65f9eb6abd6c
Venglas <bartek.bober97@gmail.com>
2019-05-08 21:32:17 +0200

    add start stop liking bot

--- extension/content-scripts/helpers/get_message_from_popup.js
+++ extension/content-scripts/helpers/get_message_from_popup.js
@@ -1,8 +1,8 @@
-const get_message_from_popup = ( message_name, function_to_do ) => {
+const get_message_from_popup = ( message_name, callback ) => {
     chrome.runtime.onMessage.addListener(
         function(request) {
             if (request.greeting == message_name) {
-                function_to_do();
+                callback();
             }
         }
     );
--- extension/content-scripts/like-bot/like_bot.js
+++ extension/content-scripts/like-bot/like_bot.js
@@ -3,6 +3,9 @@ import get_message_from_popup from '../helpers/get_message_from_popup';
 import like from './like';
 
 let data = null;
+const like_bot_data = {
+    interval: null
+};
 
 const interval = setInterval(() => {
     if ( document.querySelector('.profile-action--yes') ){
@@ -12,7 +15,11 @@ const interval = setInterval(() => {
 }, 1000);
 
 get_message_from_popup('start_liking', () => {
-    setInterval(() => {
+    like_bot_data.interval = setInterval(() => {
         like(document.querySelector('.profile-action--yes'), data);
-    }, 400);
+    }, 400); // this time offset should be setable from popup as 2 or 3 breakepoint.
+});
+
+get_message_from_popup('stop_liking', () => {
+    clearInterval( like_bot_data.interval );
 });
