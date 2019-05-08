f2275ee8e00607126cbe592102e1a5f50e371f3c
Venglas <bartek.bober97@gmail.com>
2019-05-09 01:24:24 +0200

    configurate toggle button

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,16 +1,35 @@
-// import Vue from 'vue';
-
-new Vue ({
+const vm = new Vue ({
     el: '#app',
     data: {
-        test: 1
+        like_bot: {
+            isStart: false
+        }
     },
 
     methods: {
-        sendMessageToContentScript(message_name){
+        toggle_liking(){
+            if (this.like_bot.isStart === false){
+                this.sendMessageToContentScript('start_liking', true);
+            } else {
+                this.sendMessageToContentScript('stop_liking', false);
+            }
+        },
+
+        sendMessageToContentScript(message_name, set_value){
             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {greeting: message_name});
             });
+
+            
+            this.toggleValue(this.like_bot.isStart = set_value);
+        },
+
+        toggleValue( which ){
+            const toggle = () => {
+                which;
+            };
+
+            return toggle();
         }
     }
 });
