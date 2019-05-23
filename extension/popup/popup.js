8b19537432187138146cceb1ba658a25fcc6da97
Venglas <bartek.bober97@gmail.com>
2019-05-23 07:40:00 +0200

    add opening badoo page

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,8 +1,8 @@
 Vue.component('Main_header', {
-    template: `
+	template: `
     <header class="main-header">
-        <h1>Badoo Spam Bot</h1>
-
+        <h1 @click="open_badoo">Badoo Spam Bot</h1>
+        
         <nav class="navigation">
             <ul class="navigation__list">
                 <li class="item">
@@ -15,72 +15,83 @@ Vue.component('Main_header', {
             </ul>
         </nav>
     </header>
-    `
+    `,
+
+	methods: {
+		open_badoo() {
+			chrome.tabs.create({ url: 'https://badoo.com' });
+		}
+	}
 });
 
-const vm = new Vue ({
-    el: '#app',
-    data: {
-        like_bot: {
-            isStart: false,
-            speed: localStorage.getItem('bot_like_speed') !== null ? parseInt( localStorage.getItem('bot_like_speed') ) : 50 
-        },
-        message_bot: {
-            isStart: false,
-            message: localStorage.getItem('last_message'),
-            dletingIsStart: false
-        }
-    },
+const vm = new Vue({
+	el: '#app',
+	data: {
+		like_bot: {
+			isStart: false,
+			speed:
+				localStorage.getItem('bot_like_speed') !== null ? parseInt(localStorage.getItem('bot_like_speed')) : 50
+		},
+		message_bot: {
+			isStart: false,
+			message: localStorage.getItem('last_message'),
+			dletingIsStart: false
+		}
+	},
 
-    methods: {
-        toggle_liking(){
-            if (this.like_bot.isStart === false){
-                this.sendMessageToContentScript('start_liking');
-                this.like_bot.isStart = true;
-            } else {
-                this.sendMessageToContentScript('stop_liking');
-                this.like_bot.isStart = false;
-            }
-        },
+	methods: {
+		open_badoo() {
+			alert();
+		},
 
-        toggle_messaging(){
-            if (this.message_bot.isStart === false){
-                this.sendMessageToContentScript('start_messaging');
+		toggle_liking() {
+			if (this.like_bot.isStart === false) {
+				this.sendMessageToContentScript('start_liking');
+				this.like_bot.isStart = true;
+			} else {
+				this.sendMessageToContentScript('stop_liking');
+				this.like_bot.isStart = false;
+			}
+		},
 
-                chrome.storage.sync.set({'last_message': this.message_bot.message});
-                localStorage.setItem('last_message', this.message_bot.message);
+		toggle_messaging() {
+			if (this.message_bot.isStart === false) {
+				this.sendMessageToContentScript('start_messaging');
 
-                this.message_bot.isStart = true;
-            } else {
-                this.sendMessageToContentScript('stop_messaging');
-                this.message_bot.isStart = false;
-            }
-        },
+				chrome.storage.sync.set({ last_message: this.message_bot.message });
+				localStorage.setItem('last_message', this.message_bot.message);
 
-        toggle_deleting_messages(){
-            if (this.message_bot.dletingIsStart === false){
-                this.sendMessageToContentScript('start_delete_all_old_messages');
-                this.message_bot.dletingIsStart = true;
-            } else {
-                this.sendMessageToContentScript('stop_delete_all_old_messages');
-                this.message_bot.dletingIsStart = false;
-            }
-        },
+				this.message_bot.isStart = true;
+			} else {
+				this.sendMessageToContentScript('stop_messaging');
+				this.message_bot.isStart = false;
+			}
+		},
 
-        sendMessageToContentScript(message_name, message_value = null){
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {greeting: message_name, value: message_value});
-            });
-        },
+		toggle_deleting_messages() {
+			if (this.message_bot.dletingIsStart === false) {
+				this.sendMessageToContentScript('start_delete_all_old_messages');
+				this.message_bot.dletingIsStart = true;
+			} else {
+				this.sendMessageToContentScript('stop_delete_all_old_messages');
+				this.message_bot.dletingIsStart = false;
+			}
+		},
 
-        set_like_speed(speed){
-            this.like_bot.speed = speed;
-            localStorage.setItem('bot_like_speed', speed);
-            this.sendMessageToContentScript('set_like_speed', speed);
-        }
-    },
+		sendMessageToContentScript(message_name, message_value = null) {
+			chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+				chrome.tabs.sendMessage(tabs[0].id, { greeting: message_name, value: message_value });
+			});
+		},
 
-    mounted(){
-        this.sendMessageToContentScript('update_statistics', {}); // should send obiect with stas
-    }
+		set_like_speed(speed) {
+			this.like_bot.speed = speed;
+			localStorage.setItem('bot_like_speed', speed);
+			this.sendMessageToContentScript('set_like_speed', speed);
+		}
+	},
+
+	mounted() {
+		this.sendMessageToContentScript('update_statistics', {}); // should send obiect with stas
+	}
 });
