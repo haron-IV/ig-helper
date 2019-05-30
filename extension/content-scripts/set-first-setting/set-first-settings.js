62e08ab5e20592bbea6e25493942049ad3edb6da
Venglas <bartek.bober97@gmail.com>
2019-05-30 12:25:32 +0200

    add set first setting

--- extension/content-scripts/set-first-setting/set-first-settings.js
+++ extension/content-scripts/set-first-setting/set-first-settings.js
@@ -2,23 +2,26 @@ import get_user_mail from './get_user_mail';
 
 const set_first_settings = () => {
 	const user_mail = get_user_mail();
-	const first_login = JSON.parse(localStorage.getItem('first_login'));
+	const first_login =
+		JSON.parse(localStorage.getItem('first_login')) !== null ? JSON.parse(localStorage.getItem('first_login')) : [];
 
-	if (localStorage.getItem('first_login') === null) {
-		localStorage.setItem('first_login', JSON.stringify([ user_mail ]));
-	} else {
-		// FIXME: this foreach not working!
-
-		for (let i = 0; i < first_login.length; i++) {
-			if (first_login[i] !== user_mail) {
-				first_login.push(user_mail);
-				localStorage.setItem('first_login', JSON.stringify(first_login));
-
-				// const data = JSON.parse(localStorage.getItem('first_login')).push(user_mail);
-				// localStorage.setItem('first_login', JSON.stringify(data));
+	const check_and_add_new_user = () => {
+		if (first_login.length === 0) {
+			localStorage.setItem('first_login', JSON.stringify([ user_mail ]));
+			localStorage.setItem('first_login_status', true);
+		} else {
+			for (let i = 0; i < first_login.length; i++) {
+				if (!first_login.includes(user_mail)) {
+					first_login.push(user_mail);
+					localStorage.setItem('first_login', JSON.stringify(first_login));
+					localStorage.setItem('first_login_status', true);
+					return;
+				}
 			}
 		}
-	}
+	};
+
+	check_and_add_new_user();
 };
 
 export default set_first_settings;
