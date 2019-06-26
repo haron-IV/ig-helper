e1f64c429b6cced0d85142bf25192dfb81cda1f4
Venglas <bartek.bober97@gmail.com>
2019-06-26 18:35:25 +0200

    add chatters to chaters gallery component

--- extension/popup/src/components/views/stats.vue
+++ extension/popup/src/components/views/stats.vue
@@ -13,37 +13,19 @@
         </ul>
       </div>
 
-      <Show-more buttonText="Show chatters">
-        <div v-if="profiles != null" class="chatters">
-          <div class="single-profile" v-for="(profile, index) in profiles" :key="index">
-            <div class="single-profile__img-holder">
-              <img :src="profile.img" class="img">
-            </div>
-
-            <div class="single-profile__overlap">
-              <h3>{{profile.userName}} {{profile.userAge}}</h3>
-              <button
-                class="button button--wiev-profile"
-                @click="openProfile(profile.profileLink)"
-              >View Profile</button>
-            </div>
-          </div>
-        </div>
-      </Show-more>
+      <Chatters-gallery></Chatters-gallery>
     </article>
   </main>
 </template>
 
 <script>
-import _ from "lodash";
-import showMore from "../Show-more.vue";
+import chattersGallery from "../Chatters-gallery.vue";
 
 export default {
-  components: { "Show-more": showMore },
+  components: { "Chatters-gallery": chattersGallery },
 
   data: {
-    stats: null,
-    profiles: null
+    stats: null
   },
 
   computed: {},
@@ -55,26 +37,6 @@ export default {
 
     numberWithComma(number) {
       return number.toLocaleString();
-    },
-
-    openProfile(profile) {
-      chrome.tabs.create({ url: `https://badoo.com${profile}` });
-    },
-
-    filteredChatters() {
-      const chatters = JSON.parse(localStorage.getItem("chatters"));
-      const filteredChatters = _.uniqBy(chatters.chatters, "img");
-
-      return filteredChatters;
-    },
-
-    clearChatters() {
-      const vm = this;
-
-      chrome.storage.sync.get(["chatters"], data => {
-        data.chatters = vm.filteredChatters();
-        chrome.storage.sync.set(data);
-      });
     }
   },
 
@@ -82,64 +44,18 @@ export default {
     chrome.storage.sync.get(["bot_stats"], stats => {
       localStorage.setItem("stats", JSON.stringify(stats));
     });
-
-    chrome.storage.sync.get(["chatters"], data => {
-      localStorage.setItem("chatters", JSON.stringify(data));
-    });
   },
 
   beforeMount() {
     this.stats = JSON.parse(localStorage.getItem("stats"));
-
-    this.profiles = this.filteredChatters(); // clear array
-
-    this.clearChatters();
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .main {
   &--statistics {
     max-width: 400px;
-  }
-}
-
-.chatters {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-
-  .single-profile {
-    width: 100px;
-    height: 100px;
-    margin: 10px;
-    position: relative;
-    z-index: 0;
-
-    &__img-holder {
-      .img {
-        width: 100%;
-      }
-    }
-
-    &__overlap {
-      position: absolute;
-      top: 0;
-      left: 0;
-      z-index: 1;
-      color: #fff;
-      background-color: rgba(0, 0, 0, 0.6);
-      text-align: center;
-      height: 100%;
-      width: 100%;
-      opacity: 0;
-      transition: opacity ease-in-out 400ms;
-
-      &:hover {
-        opacity: 1;
-      }
-    }
   }
 }
 </style>
