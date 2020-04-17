0aa87ef234fe6546f53470dd857cd3adc25e1292
Bartek <bartek.bober.koder@gmail.com>
2020-04-17 19:55:33 +0200

    router config

--- extension/popup/src/views/Home.vue
+++ extension/popup/src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <v-container class="my-2">
+    <v-layout row>
+      <v-flex xs>
+        <h1>elo</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
