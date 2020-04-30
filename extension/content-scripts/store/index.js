d440edc1fa6580bba09376dc69a5cb69c1c9f003
Bartek <bartek.bober.koder@gmail.com>
2020-04-30 02:44:08 +0200

    fix reinitialize chrome storage on page refresh

--- extension/content-scripts/store/index.js
+++ extension/content-scripts/store/index.js
@@ -2,8 +2,16 @@ import storeSchema from "./storeSchema";
 import { setData } from "./chromeStore";
 
 const checkIsStoreInitialized = (store) => {
-    if (Object.keys(store).length <= 1) return false;
-    return true
+    if ( store.igHelperStore &&  store.igHelperStore.initialized === false) {
+        store.igHelperStore.initialized = true;
+        updateStore(store);
+        return false;
+    }
+    return true;
+
+
+    // if (Object.keys(store).length <= 1) return false;
+    // return true
 
     // TODO: Make it better ;)
     // if ( Object.keys(store).length <= 1 ) {
