fac2657670d76fec2864588fb1a271b97b3e1669
Bartek <bartek.bober.koder@gmail.com>
2020-05-04 23:08:52 +0200

    fix menu item clicabe area

--- extension/popup/src/App.vue
+++ extension/popup/src/App.vue
@@ -26,12 +26,13 @@
           v-for="route in $router.options.routes"
           :key="route.name"
           link
+          @click="$router.push(route.path)"
         >
           <v-list-item-icon>
             <v-icon>{{ route.menuIcon }}</v-icon>
           </v-list-item-icon>
 
-          <v-list-item-content class="space-item" @click="$router.push(route.path)">
+          <v-list-item-content class="space-item">
             <v-list-item-title>{{ route.name }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
