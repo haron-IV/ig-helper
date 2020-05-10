5f17eb2e47163cb04693c3b42e1a477512cdfa74
Bartek <bartek.bober.koder@gmail.com>
2020-05-10 17:21:48 +0200

    add unfollowing after scrolling list

--- extension/content-scripts/unfollow-profiles/loadProfiles.js
+++ extension/content-scripts/unfollow-profiles/loadProfiles.js
@@ -1,17 +1,30 @@
-const loadProfiles = (howMuch) => {
-    const scrollCount = howMuch / 12;
+
+
+const loadProfiles = (howMuch, callback) => {
+    const scrollCount = JSON.parse(howMuch) / 12;
     let i = 0;
-    scroll(scrollCount, i)
+    
+    scroll(scrollCount, i).then( result => {
+        callback();
+    })
 }
 
 const scroll = (scrollCount, i) => {
-    if (i < scrollCount) {
-        document.querySelector("body > div.RnEpo.Yx5HN > div > div.isgrP").scrollTop += 1000;
-        i++;
-        console.log(`Loaded profiles: ${document.querySelector("body > div.RnEpo.Yx5HN > div > div.isgrP > ul > div").children.length} | i: ${i}`);
-        
-        setTimeout(() => {
-            scroll(scrollCount, i);    
+    const scrollingPromise = new Promise( (resolve, reject) => {
+        const interval = setInterval(() => {
+            document.querySelector("body > div.RnEpo.Yx5HN > div > div.isgrP").scrollTop += 1000;
+            i++;
+            console.log(`Loaded profiles: ${document.querySelector("body > div.RnEpo.Yx5HN > div > div.isgrP > ul > div").children.length} | i: ${i} | scrollCount: ${scrollCount}`);
+            if (i >= scrollCount) {
+                clearInterval(interval);
+                resolve("loadedProfilesDone");
+            };
         }, 2000);
-    }
+    });
+    
+    return scrollingPromise;
 };
+
+export default loadProfiles;
+
+// console script need to implement in content scrpit
