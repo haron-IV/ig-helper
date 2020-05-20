d64ba39412adb6a588ccc5c10371217aebeebacd
Mistrz <bartek.bober.koder@gmail.com>
2020-05-21 01:40:25 +0200

    add base for GA

--- extension/popup/src/App.vue
+++ extension/popup/src/App.vue
@@ -82,8 +82,21 @@ export default {
         this.showApp = true;
         this.remoteVersion = res.data.version;
       } else { this.needUpdate = true; }
-    })
-  },
+    });
+    //
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-167301033-1', 'auto');
+
+// Modifications:
+    ga('set', 'checkProtocolTask', null); // Disables file protocol checking.
+    ga('send', 'pageview', '/popup'); // Set page, avoiding rejection due to chrome-extension protocol
+
+},
   watch: {
     '$store.getters.getLastMenuPosition'(){
       this.$router.push(this.$store.getters.getLastMenuPosition);
